ab20428f50019aeddc7d6d2466938e1df12865c6
AryanKulkarni <kulkarni.arya@northeastern.edu>
2024-11-21 05:06:10 -0500

    Assignment 10 edge detection

--- Assignment10_fbo/part1/shaders/fboFrag.glsl
+++ Assignment10_fbo/part1/shaders/fboFrag.glsl
@@ -2,22 +2,44 @@
 #version 330 core
 
 // ======================= uniform ====================
-// If we have texture coordinates, they are stored in this sampler.
-uniform sampler2D u_DiffuseMap; 
+uniform sampler2D u_DiffuseMap;
 
 // ======================= IN =========================
-in vec2 v_texCoord; // Import our texture coordinates from vertex shader
+in vec2 v_texCoord; // Import our texture coordinates from the vertex shader
 
 // ======================= out ========================
-// The final output color of each 'fragment' from our fragment shader.
 out vec4 FragColor;
+
+const float offset = 1.0 / 300.0; 
 
 void main()
 {
-    // Store our final texture color
-    vec3 diffuseColor;
-    diffuseColor = texture(u_DiffuseMap, v_texCoord).rgb;
-        
-    FragColor = vec4(diffuseColor,1.0);
+    vec2 offsets[9] = vec2[](
+        vec2(-offset,  offset), // top-left
+        vec2( 0.0f,    offset), // top-center
+        vec2( offset,  offset), // top-right
+        vec2(-offset,  0.0f),   // center-left
+        vec2( 0.0f,    0.0f),   // center-center
+        vec2( offset,  0.0f),   // center-right
+        vec2(-offset, -offset), // bottom-left
+        vec2( 0.0f,   -offset), // bottom-center
+        vec2( offset, -offset)  // bottom-right    
+    );
+
+    float kernel[9] = float[](
+        1.0,  1.0,  1.0,
+        1.0, -8.0,  1.0,
+        1.0,  1.0,  1.0
+    );
+
+    vec3 sampleTex[9];
+    for(int i = 0; i < 9; i++)
+    {
+        sampleTex[i] = texture(u_DiffuseMap, v_texCoord + offsets[i]).rgb;
+    }
+    vec3 col = vec3(0.0);
+    for(int i = 0; i < 9; i++)
+        col += sampleTex[i] * kernel[i];
+
+    FragColor = vec4(col, 1.0);
 }
-// ==================================================================
